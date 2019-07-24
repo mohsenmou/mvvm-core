c564620dfc8ac8a4eec0ffcc23e5a46ee68837a1
mohsen mousavi <mohsenmou@gmail.com>
2019-07-24 17:08:36 +0430

    modify NotifyDataErrorInfoBase class

--- Mohsenmou.MVVM/Mohsenmou.MVVM.Core/NotifyDataErrorInfoBase.cs
+++ Mohsenmou.MVVM/Mohsenmou.MVVM.Core/NotifyDataErrorInfoBase.cs
@@ -8,39 +8,35 @@ namespace Mohsenmou.MVVM.Core
 {
     public class NotifyDataErrorInfoBase : ViewModelBase, INotifyDataErrorInfo
     {
-        private Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
+        protected readonly Dictionary<string, List<string>> Errors;
+        protected NotifyDataErrorInfoBase()
+        {
+            Errors = new Dictionary<string, List<string>>();
+        }
+
         public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public bool HasErrors => Errors.Any();
+
         public IEnumerable GetErrors(string propertyName)
         {
-            return _errorsByPropertyName.ContainsKey(propertyName)
-                ? _errorsByPropertyName[propertyName]
-                : null;
+            return propertyName != null && Errors.ContainsKey(propertyName)
+                ? Errors[propertyName]
+                : Enumerable.Empty<string>();
         }
-        protected virtual void AddError(string propertyName, string error)
+
+        protected void ClearErrors()
         {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
+            foreach (var propertyname in Errors.Keys.ToList())
             {
-                _errorsByPropertyName[propertyName] = new List<string>();
-            }
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
+                Errors.Remove(propertyname);
+                OnErrorsChanged(propertyname);
             }
         }
-        protected virtual void ClearErrors(string propertyName)
-        {
-            if (_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName.Remove(propertyName);
-                OnErrorsChanged(propertyName);
-            }
-        }
+
         protected virtual void OnErrorsChanged(string propertyName)
         {
             ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-            base.OnPropertyChanged(nameof(HasErrors));
         }
     }
 }
