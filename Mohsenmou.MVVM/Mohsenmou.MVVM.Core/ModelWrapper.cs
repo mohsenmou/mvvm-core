d4f2ffb7d6bbb81d8b1261b5e0e1b04a5bf939c9
mohsen mousavi <mohsenmou@gmail.com>
2019-07-24 17:08:58 +0430

    implement ModelWrapper class

---
+++ Mohsenmou.MVVM/Mohsenmou.MVVM.Core/ModelWrapper.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Mohsenmou.MVVM.Core
+{
+    public class ModelWrapper<T> : NotifyDataErrorInfoBase, IValidatableTrackingObject, IValidatableObject
+    {
+        private readonly List<IValidatableTrackingObject> _trackingObjects;
+        private readonly Dictionary<string, object> _originalValues;
+
+        public ModelWrapper(T model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException("model");
+            }
+            Model = model;
+            _originalValues = new Dictionary<string, object>();
+            _trackingObjects = new List<IValidatableTrackingObject>();
+            InitializeComplexProperties(model);
+            InitializeCollectionProperties(model);
+            Validate();
+        }
+
+        public bool IsChanged => _originalValues.Count > 0 || _trackingObjects.Any(t => t.IsChanged);
+
+        public bool IsValid => !HasErrors && _trackingObjects.All(t => t.IsValid);
+
+        public T Model { get; }
+
+        public void AcceptChanges()
+        {
+            _originalValues.Clear();
+            foreach (var trackingObject in _trackingObjects)
+            {
+                trackingObject.AcceptChanges();
+            }
+            OnPropertyChanged(nameof(IsChanged));
+            OnPropertyChanged("");
+        }
+
+        public void RejectChanges()
+        {
+            foreach (var originalValueEntry in _originalValues)
+            {
+                typeof(T).GetProperty(originalValueEntry.Key).SetValue(Model, originalValueEntry.Value);
+            }
+            _originalValues.Clear();
+            foreach (var trackingObject in _trackingObjects)
+            {
+                trackingObject.RejectChanges();
+            }
+            Validate();
+            OnPropertyChanged("");
+        }
+
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
+
+        protected bool GetIsChanged(string propertyName)
+        {
+            return _originalValues.ContainsKey(propertyName);
+        }
+
+        protected virtual TValue GetOriginalValue<TValue>(string propertyName)
+        {
+            return _originalValues.ContainsKey(propertyName)
+                ? (TValue)_originalValues[propertyName]
+                : GetValue<TValue>(propertyName);
+        }
+
+        protected virtual TValue GetValue<TValue>([CallerMemberName]string propertyName = null)
+        {
+            var propertyInfo = Model.GetType().GetProperty(propertyName);
+            return (TValue)propertyInfo.GetValue(Model);
+        }
+        protected virtual void InitializeCollectionProperties(T model)
+        {
+        }
+
+        protected virtual void InitializeComplexProperties(T model)
+        {
+        }
+
+        protected void RegisterCollection<TWrapper, TModel>(ChangeTrackingCollection<TWrapper> wrapperCollection,
+                        List<TModel> modelCollection)
+                        where TWrapper : ModelWrapper<TModel>
+        {
+            wrapperCollection.CollectionChanged += (s, e) =>
+            {
+                modelCollection.Clear();
+                modelCollection.AddRange(wrapperCollection.Select(w => w.Model));
+            };
+            RegisterTrackingObject(wrapperCollection);
+        }
+
+        protected void RegisterComplex<TModel>(ModelWrapper<TModel> wrapper)
+        {
+            RegisterTrackingObject(wrapper);
+        }
+
+        protected virtual void SetValue<TValue>(TValue newValue, [CallerMemberName]string propertyName = null)
+        {
+            var propertyInfo = Model.GetType().GetProperty(propertyName);
+            var currentValue = propertyInfo.GetValue(Model);
+            if (!Equals(currentValue, newValue))
+            {
+                UpdateOriginalValue(currentValue, newValue, propertyName);
+                propertyInfo.SetValue(Model, newValue);
+                Validate();
+                OnPropertyChanged(propertyName);
+                OnPropertyChanged(propertyName + "IsChanged");
+            }
+        }
+        protected virtual IEnumerable<string> ValidateProperty(string propertyName)
+        {
+            return null;
+        }
+
+        private void RegisterTrackingObject(IValidatableTrackingObject trackingObject)
+
+        {
+            if (!_trackingObjects.Contains(trackingObject))
+            {
+                _trackingObjects.Add(trackingObject);
+                trackingObject.PropertyChanged += TrackingObjectPropertyChanged;
+            }
+        }
+
+        private void TrackingObjectPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(IsChanged))
+            {
+                OnPropertyChanged(nameof(IsChanged));
+            }
+            else if (e.PropertyName == nameof(IsValid))
+            {
+                OnPropertyChanged(nameof(IsValid));
+            }
+        }
+
+        private void UpdateOriginalValue(object currentValue, object newValue, string propertyName)
+        {
+            if (!_originalValues.ContainsKey(propertyName))
+            {
+                _originalValues.Add(propertyName, currentValue);
+                OnPropertyChanged("IsChanged");
+            }
+            else
+            {
+                if (Equals(_originalValues[propertyName], newValue))
+                {
+                    _originalValues.Remove(propertyName);
+                    OnPropertyChanged("IsChanged");
+                }
+            }
+        }
+
+        private void Validate()
+        {
+            ClearErrors();
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(this);
+            Validator.TryValidateObject(this, context, results, true);
+
+            if (results.Any())
+            {
+                var propertyNames = results.SelectMany(r => r.MemberNames).Distinct().ToList();
+                foreach (var propertyName in propertyNames)
+                {
+                    Errors[propertyName] = results
+                        .Where(r => r.MemberNames.Contains(propertyName))
+                        .Select(r => r.ErrorMessage)
+                        .Distinct()
+                        .ToList();
+                    OnErrorsChanged(propertyName);
+                }
+            }
+            OnPropertyChanged(nameof(IsValid));
+        }
+    }
+}
